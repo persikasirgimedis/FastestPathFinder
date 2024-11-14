5cddab83f4ecae7df832e1a82b5cb19153287558
butk <butk.andrius@gmail.com>
2024-11-14 21:28:58 +0200

    Remove legacy unit test with wrong result

--- PathFinderTests/FastestPathFinderTests.cs
+++ PathFinderTests/FastestPathFinderTests.cs
@@ -1,13 +1,6 @@
 [TestFixture]
 public class FastestPathFinderTests
 {
-    private string _ltPath = @"Klaipeda,Kaunas,2:00
-Vilnius,Kaunas,1:00
-Kaunas,Ukmerge,1:00
-Vilnius,Ukmerge,1:00
-Klaipeda,Panevezys,3:00
-Panevezys,Vilnius,2:00";
-
     [Test]
     public async Task FindFastestPathDuration()
     {
@@ -31,15 +24,6 @@ Panevezys,Vilnius,2:00";
         Assert.NotNull(vertex);
         var paths = string.Join(',', vertex.GetDirections());
         Assert.AreEqual("Vilnius,Kaunas,Klaipeda", paths);
-    }
-
-    [Test]
-    public async Task FindFastestPath_LTPathArray_Bug()
-    {
-        var finder = new PathFinder.PathFinder();
-        var result = await finder.FindFastestPath("Vilnius", "Klaipeda", _ltPath);
-        Assert.AreEqual(TimeSpan.FromMinutes(210), result.Item1);
-        Assert.AreEqual("Vilnius,Kaunas,Klaipeda", result.Item2);
     }
 
     private static FastestPathFinder SetupFinderAndPopulateRoutes()
